1c01887d7bc0101931baff42d751e47b6f219428
Ruben Salgado <aluno21374@ipt.pt>
2020-05-01 13:09:14 +0100

    Iniciação das Classes(Muito por fazer ainda)

---
+++ TicketEagle/TicketEagle/Models/Bilhete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketEagle.Models
+{
+    public class Bilhete
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string NomeEvento { get; set; }
+
+        public string Descrição { get; set; }
+
+        [ForeignKey("Local")]
+        public int LocalFK { get; set; }
+
+        public string Data { get; set; }
+
+
+    }
+}
---
+++ TicketEagle/TicketEagle/Models/Utilizador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketEagle.Models
+{
+    public class Utilizador
+    {
+        public Utilizador()
+        {
+            ListaBilhetes = new HashSet<Bilhete>();
+        }
+
+        [Key]
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Password { get; set; }
+
+        public string Foto { get; set; }
+
+        public virtual ICollection<Bilhete> ListaBilhetes { get; set; }
+      
+    }
+
+  
+}
